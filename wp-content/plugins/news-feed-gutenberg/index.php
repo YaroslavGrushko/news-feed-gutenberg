924062b1b61b57f03fedfae991e47f7917f19756
YaroslavGrushko <Grushko.kpi@gmail.com>
2023-11-15 16:09:08 +0200

    Gutenberg-news: example init

--- wp-content/plugins/news-feed-gutenberg/index.php
+++ wp-content/plugins/news-feed-gutenberg/index.php
@@ -1,31 +1,8 @@
 <?php 
 /**
- * Plugin Name: News Feed Gutenberg
- * Plugin URI: https://yvg.com.ua/
- * Description: News feed (Gutenberg block) using data from the API and optimized for server rendering (SSR).
- * Version: 1.0.0
- * Author: Yaroslav Grushko
- * Author URI: https://yvg.com.ua/
- * Text Domain: news-feed-gutenberg
- * Domain Path: /languages/
+ * Plugin Name: Gutenberg examples 01
  */
-
-if( !defined('NEWS_FEED_GUTENBERG_VERSION') ){
-	define('NEWS_FEED_GUTENBERG_VERSION', '1.0.0');
+function news_feed_gutenberg_01_register_block() {
+    register_block_type( __DIR__ );
 }
-
-if( !defined('NEWS_FEED_GUTENBERG_VERSION') ){
-	define('NEWS_FEED_GUTENBERG_VERSION', plugin_dir_path( __FILE__ ));
-}
-
-class News_Feed_Gutenberg{
-    function __construct(){
-        // register news block
-		add_action( 'init', array($this, 'news_feed_gutenberg_register_block') );
-	}
-	function news_feed_gutenberg_register_block() {
-		register_block_type( __DIR__ );
-	}
-}
-
-new News_Feed_Gutenberg();
+add_action( 'init', 'news_feed_gutenberg_01_register_block' );
--- wp-content/plugins/news-feed-gutenberg/src/index.js
+++ wp-content/plugins/news-feed-gutenberg/src/index.js
@@ -4,9 +4,9 @@
  import { registerBlockType } from '@wordpress/blocks';
 
  // Register the block
- registerBlockType( 'news-feed-gutenberg/news', {
+ registerBlockType( 'news-feed-gutenberg/example-01-basic-esnext', {
      edit: function () {
-         return <p> Hello world (from the editor)</p>;
+         return <p> Hello world. (from the editor)</p>;
      },
      save: function () {
          return <p> Hola mundo (from the frontend) </p>;
